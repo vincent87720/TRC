9fe21ce0b47cf949a50d5f96c0d9f89d4d2823fd
vincent87720 <vincent87720@yahoo.com.tw>
2022-03-13 15:24:14 +0800

    Add: makefile;

---
+++ make.bat
@@ -0,0 +1,14 @@
+SET CGO_ENABLED=0
+SET GOOS=darwin
+SET GOARCH=amd64
+go build -o .\bin\darwin\trc .\cmd\trc
+
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=amd64
+go build -o .\bin\linux\trc .\cmd\trc
+
+SET CGO_ENABLED=0
+SET GOOS=windows
+SET GOARCH=amd64
+go build -o .\bin\windows\trc.exe .\cmd\trc
